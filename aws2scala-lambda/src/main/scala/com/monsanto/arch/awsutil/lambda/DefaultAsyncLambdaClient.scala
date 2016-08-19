793493c1c1dcdd87ec2559599e95ae367b126905
zomboc0m <skohner1@gmail.com>
2016-08-19 11:36:06 -0500

    both getFunction methods provided

--- aws2scala-lambda/src/main/scala/com/monsanto/arch/awsutil/lambda/DefaultAsyncLambdaClient.scala
+++ aws2scala-lambda/src/main/scala/com/monsanto/arch/awsutil/lambda/DefaultAsyncLambdaClient.scala
@@ -2,12 +2,17 @@ package com.monsanto.arch.awsutil.lambda
 
 import akka.stream.Materializer
 import akka.stream.scaladsl.{Sink, Source}
-import com.monsanto.arch.awsutil.lambda.model.GetFunctionRequest
+import com.monsanto.arch.awsutil.lambda.model.{FunctionArn, GetFunctionRequest}
 
 /**
   * Created by stevenkohner on 8/16/16.
   */
 private[awsutil] class DefaultAsyncLambdaClient(streaming: StreamingLambdaClient) extends AsyncLambdaClient {
   override def getFunction(functionName: String)(implicit m: Materializer) =
-    Source.single(GetFunctionRequest(functionName)).via()
+    Source.single(GetFunctionRequest(functionName))
+      .via(streaming.functionGetter)
+      .runWith(Sink.head)
+
+  override def getFunction(functionArn: FunctionArn)(implicit m: Materializer) =
+    getFunction(functionArn.toString)(m)
 }
